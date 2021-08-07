3001ddc5b14826e2cf6c5701ca2268c6a6135d63
Sourish <54475400+sourish279@users.noreply.github.com>
2021-08-07 13:21:27 +1000

    Dashboard theme and googleSheets

--- SeasideSide/app.R
+++ SeasideSide/app.R
@@ -1,32 +1,84 @@
+library(tidyverse)
+library(DT)
+library(plotly)
 
 
 library(shiny)
+library(shinydashboard)
+library(googlesheets4)
+library(googledrive)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
 
+driveToken = googledrive::drive_auth(email = c("sourish.iyengar@gmail.com"), path = "https://drive.google.com/drive/folders/1PGMilQ7u0zDQ-KJbplDHxG5I-en5IMds")
+sheetsToken = gs4_auth(token = drive_token())
+
+
+
+files = drive_ls( "https://drive.google.com/drive/folders/1PGMilQ7u0zDQ-KJbplDHxG5I-en5IMds")
+
+
+
+ui <- dashboardPage(
+    
+    # Colour
+    skin = "blue",
+    
     # Application title
-    titlePanel("Seaside Dashboard"),
+    dashboardHeader(title = "Seashine"),
+    
+    # Sidebar Menu
+    dashboardSidebar(    
+        sidebarMenu(
+        menuItem("Pre-event", tabName = "preEvent", icon = icon("")),
+        menuItem("Post-event", tabName = "postEvent", icon = icon("")),
+        menuItem("Stratified-Analysis", tabName = "stratifiedAnalysis", icon = icon(""))
+        )
+    ),
+    
+    # Main Body
+    dashboardBody(
+        # Boxes need to be put in a row (or column)
+        fluidRow(
+            column(4,
+                box(
+                    status = "primary", 
+                    solidHeader = TRUE,
+                    collapsible = TRUE,
+                    title = "Controls",
+                    width = 12,
 
-
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        mainPanel(
-           plotOutput("distPlot")
+                    # Panel 1 sidebar
+                    sidebarPanel(
+                        width = 12,
+                        selectizeInput("sheet", "Choose Survey", choices = files$name)
+                    ),
+                )
+            ),
+            
+            column(8,
+                box(title = "Data", 
+                    status = "primary", 
+                    solidHeader = TRUE,
+                    collapsible = TRUE,
+                    width = 12,
+                    
+                    # Panel 1 Data
+                    DT::DTOutput("googleSheetData", height = 250)
+                    )
+            )
+            
         )
     )
+    
+    
+    
 )
 
 
-server <- function(input, output) {
-
+server <- function(input, output, server) {
+    
+    
+    output$googleSheetData = renderDT({read_sheet(drive_get(input$sheet))})
 
 }
 
