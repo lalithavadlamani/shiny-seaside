b534e9d75fca9c5abfe1d5fb018f1f5d2109711a
Lalitha Vadlamani <35949189+lalithavadlamani@users.noreply.github.com>
2021-10-13 20:05:42 +1100

    Update KPI_vs_Year.R

--- 3_Visualisation_Functions/kpi_over_years_viz/KPI_vs_Year.R
+++ 3_Visualisation_Functions/kpi_over_years_viz/KPI_vs_Year.R
@@ -2,7 +2,7 @@ library(ggplot2)
 library('readxl')
 library(reshape2)
 
-data <- read_excel("C:/Users/Lalitha Vadlamani/Desktop/180 DC/S2/year_dummy.xlsx")
+data <- read_excel("C:/Users/Lalitha Vadlamani/Desktop/180 DC/S2/kpi_over_years_viz/year_dummy.xlsx")
 
 
 all_year_plot <- function(data){
@@ -14,13 +14,15 @@ all_year_plot <- function(data){
   df <- reshape2::melt(data, id.var = var4)
   
   plot <- ggplot(df, aes(x = year, y = value, colour = variable)) + 
-    geom_point() + 
+    geom_point(color='black') + 
     geom_line() + 
     xlab('Year') + 
     ylab('KPI value') +
+    scale_y_continuous(limits=c(0,1),breaks = seq(0, 1, by = 0.1)) +
     ggtitle("KPI's over the years") +
     guides(color = guide_legend(title = "KPI")) + 
-    scale_color_discrete(labels = c('Action','Learning','Community'))
+    scale_color_discrete(labels = c('Action','Learning','Community')) + 
+    theme_classic()
   
   return(plot)
 }
